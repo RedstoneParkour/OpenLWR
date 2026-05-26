742ecc371093aa1c77f6aaf7bb44c231525c28c6
kcinnaJlol <39594309+kcinnaJlol@users.noreply.github.com>
2026-05-26 20:11:20 +0200

    add support for selector switch flags
    including the most complex ack-checking system to prevent sounds from
    updates for things our client already did

--- Assets/Common/Switches/Selector Switch/selector_switch.gd
+++ Assets/Common/Switches/Selector Switch/selector_switch.gd
@@ -9,100 +9,76 @@ enum SwitchFlag {
 	RED,
 }
 
-enum RotateOpposite {
-	NO,
-	UNSPECIFIED,
-	YES,
-}
-
 @onready var player = $"/root/Node3D/Player"
-@export var switch_position: int = 0
+@export var switch_neutral_position: int = 0
+var switch_position: int = 0
 @export var switch_positions: Dictionary[int, float]
 var switch_flag: SwitchFlag
 var switch_local_push: bool = false
+#array of two ints: [0] is the target position, [1] is the deadline (engine ticks, milliseconds)
+var unacked_client_changes: Array[PackedInt64Array] = []
+const CHANGE_ACK_TIMEOUT := 1000 # milliseconds
 @export var switch_momentary: bool = false
-@export var rotate_opposite: RotateOpposite = RotateOpposite.UNSPECIFIED
 @onready var has_flag = get_node_or_null("selector_switch/Flag")
-var flag_green = null
-var flag_red = null
+var flag_green = preload("res://Assets/Materials/green_flag.tres")
+var flag_red = preload("res://Assets/Materials/red_flag.tres")
 
-#func init():
-	#switch = node_3d.switches[self.name]
-	#switch.switch = self
-	#switch.local_push = false
-	#player.unclick_left.connect(switch_unclick)
-	#if switch.lights != {}:
-		#for light in switch.lights:
-			#if light == "green" or light == "red":
-				##this is so we dont have to make every light unique
-				#var light_material = get_node(light+"/Lamp").get_material().duplicate()
-				#get_node(light+"/Lamp").material = light_material
-				#light_material.emission_enabled = switch["lights"][light]
-				#switch["lights"][light] = light_material
-			#else:
-				#var light_material = get_node(light).get_material().duplicate()
-				#get_node(light).material = light_material
-				#light_material.emission_enabled = switch["lights"][light]
-				#switch["lights"][light] = light_material
-				#
-	#if has_flag:
-		#switch["flag"] = "green"
-		##preload the materials here, if it has a flag
-		#flag_green = preload("res://Assets/Materials/green_flag.tres")
-		#flag_red = preload("res://Assets/Materials/red_flag.tres")
-		#
-	#switch_position_change(switch["position"],true)
-
-func _flag_to_string(flag: SwitchFlag):
-	match flag:
-		SwitchFlag.GREEN:
-			return "green"
-		SwitchFlag.RED:
-			return "red"
-
-func _string_to_flag(flag: String):
-	match flag:
-		"green":
-			return SwitchFlag.GREEN
-		"red":
-			return SwitchFlag.RED
 
 func _ready():
+	switch_position = switch_neutral_position
 	if netname == &"":
 		netname = StringName(name)
 		push_warning("netname property of selector switch is empty, using node name %s" % netname)
-	if rotate_opposite == RotateOpposite.UNSPECIFIED:
-		rotate_opposite = RotateOpposite.YES if has_node("rotate_opposite") else RotateOpposite.NO
-		push_warning("rotate_opposite property of switch %s left unspecified, using node check" % netname)
 	id = Network.name_to_id(netname)
 	Network.register_device(id, self.get_path())
 	player.unclick_left.connect(switch_unclick)
 	switch_model_update(true)
 
+func _push_client_update(new: int) -> void:
+	var now = Time.get_ticks_msec()
+	var deadline = now + CHANGE_ACK_TIMEOUT
+	unacked_client_changes.append(PackedInt64Array([new, deadline]))
+	while unacked_client_changes.size() > 0:
+		var change = unacked_client_changes[0]
+		if change[1] < now:
+			unacked_client_changes.remove_at(0)
+		else:
+			break
+
+func _is_client_update(new: int) -> bool:
+	var now := Time.get_ticks_msec()
+	var ret := false
+	while unacked_client_changes.size() > 0:
+		var change = unacked_client_changes[0]
+		if change[0] == new:
+			ret = true
+			unacked_client_changes.remove_at(0)
+			break
+		elif change[1] < now:
+			unacked_client_changes.remove_at(0)
+		else:
+			break
+	return ret
+
 func net_update(info):
 	if info is Dictionary:
+		var nosound = true
+		var do_update = false
 		if 0 in info: # switch position
-			var do_sound = not (switch_position == info[0])
+			nosound = nosound and _is_client_update(info[0])
+			do_update = true
 			switch_position = info[0]
-			switch_model_update(do_sound)
+		if 1 in info: # switch flag
+			do_update = true
+			switch_flag = SwitchFlag.RED if info[1] else SwitchFlag.GREEN
+		if do_update:
+			switch_model_update(nosound)
 	pass
-
-func switch_update(info: Dictionary):
-	if "position" in info:
-		switch_position = info.position
-	if "positions" in info:
-		switch_positions = info.positions
-	if "flag" in info:
-		switch_flag = _string_to_flag(info.flag)
-	switch_model_update()
 
 func switch_model_update(nosound: bool = false):
 	var rotate_position = switch_positions.get(switch_position, 0)
 	var handle_rotation = round($"selector_switch/Handle".rotation_degrees.y)
 	
-	# used in the case where a switch was modeled such that it needs to be rotated the opposite direction
-	if rotate_opposite == RotateOpposite.YES:
-		rotate_position = rotate_position * -1
 	if handle_rotation != rotate_position:
 		if not nosound:
 			$"Move".playing = true
@@ -117,9 +93,9 @@ func switch_model_update(nosound: bool = false):
 				has_flag.set_surface_override_material(0,flag_red)
 
 func _client_switch_position_change(to_position: int, no_sound: bool = false):
-	switch_flag = SwitchFlag.RED if to_position >= 0 else SwitchFlag.GREEN
 	switch_position = to_position
 	switch_model_update(no_sound)
+	_push_client_update(to_position)
 	
 	const UBC = preload("res://Assets/Generated/Protocols/ubc.gd")
 	var update_data = UBC.UBCMessage.Payload.Data.new()
@@ -131,7 +107,6 @@ func switch_click_left(_camera, event, _position, _normal, _shape_idx):
 	var mouse_click = event as InputEventMouseButton
 	if mouse_click and mouse_click.button_index == 1:
 		if mouse_click.pressed and (switch_position+1 in switch_positions):
-			print("swotch")
 			switch_local_push = true
 			_client_switch_position_change(switch_position+1)
 			
@@ -145,7 +120,7 @@ func switch_click_right(_camera, event, _position, _normal, _shape_idx):
 			
 func switch_unclick():
 	if switch_local_push and switch_momentary:
-		_client_switch_position_change(1)
+		_client_switch_position_change(switch_neutral_position)
 
 func switch_click_ptl(_camera, event, _position, _normal, _shape_idx):
 	print("pull to lock") #TODO
